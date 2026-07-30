edf604de00361bfd2a177e034c47a61ef68615e3
diego-hernandez-jimenez <diego.her.jimenez@gmail.com>
2026-07-30 18:54:08 +0200

    project reorganization

--- app/src/train_model.py
+++ app/src/train_model.py
@@ -1,8 +1,8 @@
 # Script to train machine learning model.
 
 from sklearn.model_selection import train_test_split
-
-# Add the necessary imports for the starter code.
+from ml.data import process_data
+from ml.model import train_model
 
 # Add code to load in the data.
 
